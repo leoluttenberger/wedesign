a27a5e4f04eca570dcd86682b836a843aa29e141
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2022-05-19 16:29:18 +0200

    Removed history, due to sensitive data

---
+++ docs/README.md
@@ -0,0 +1,71 @@
+# wedesign
+## Depedencies
+```
+# Nodejs using Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Vuejs 
+npm install -g @vue/cli
+
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Run your end-to-end tests
+```
+npm run test:e2e
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+## Capasitor 
+### Seutp
+```
+ npx cap init
+```
+```
+npx cap add android
+```
+```
+npm run build
+```
+```
+npx cap copy
+```
+npx cap run android
+```
+
+## Firebase
+
+### Firebase Config
+```
+firebase login
+```
+```
+firebase init hosting
+```
+```
+firebase init deploy
+```
+### Firebase env
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/"> Home </router-link> |
+      <router-link to="/feed"> Feed </router-link> |
+      <span v-if="isLoggedIn">
+        <button @click="signOut">Logout</button>
+      </span>
+      <span v-else>
+        <router-link to="/register"> Register </router-link> |
+        <router-link to="/sign-in"> Login </router-link>
+      </span>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { getAuth } from "firebase/auth";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const isLoggedIn = ref(true);
+const auth = getAuth();
+
+// runs after firebase is initialized
+auth.onAuthStateChanged(function (user) {
+  if (user) {
+    isLoggedIn.value = true; // if we have a user
+  } else {
+    isLoggedIn.value = false; // if we do not
+  }
+});
+const signOut = () => {
+  auth.signOut();
+  router.push("/");
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

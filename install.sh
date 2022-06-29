386463794b8d0db856bc940ecab83f8d9ef070b4
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2022-06-29 04:43:10 +0200

    added fastfile.example

--- install.sh
+++ install.sh
@@ -17,15 +17,10 @@ echo "Init Firebase config"
 echo "======================================================================================================"
 sh initFirebaseConfig.sh
 echo "======================================================================================================"
-echo "Configure and build android app"
+echo "Configure and build android app plus fastlane"
 echo "======================================================================================================"
 cp build.gradle.example.android.app $( pwd; )/android/app/
 mv $( pwd; )/android/app/build.gradle.example.android.app $( pwd; )/android/app/build.gradle
-cd android && gradle build
-echo "======================================================================================================"
-echo "Create and install fastlane android distribution"
-echo "======================================================================================================"
-fastlane init
-sudo fastlane add_plugin firebase_app_distribution
+cd android && gradle build && fastlane init && sudo fastlane add_plugin firebase_app_distribution
 
 

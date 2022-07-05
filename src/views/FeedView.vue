93816923502dfbef4f1c10b3eefb88027fdf08af
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2022-07-05 09:47:24 +0200

    initial commit

---
+++ src/views/FeedView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Feed</h1>
+  <!--<img src="../assets/fabian.jpg" /> -->
+  <!--  <img src="../assets/simon.jpg" /> -->
+  <!-- <img src="../assets/felix.jpg" /> -->
+  <!--<img src="../assets/nicole.jpg" /> -->
+</template>
+
+<script setup>
+import { getAuth } from "firebase/auth";
+import { useRouter } from "vue-router";
+import { onBeforeUnmount } from "vue";
+const router = useRouter();
+const auth = getAuth();
+const authListener = auth.onAuthStateChanged(function (user) {
+  if (!user) {
+    // not logged in
+    alert("you must be logged in to view this. redirecting to the home page");
+    router.push("/");
+  }
+});
+onBeforeUnmount(() => {
+  // clear up listener
+  authListener();
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Home</h1>
+  <img src="../assets/logo.png" />
+</template>

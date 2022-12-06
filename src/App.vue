316b30eedb06a01113bc8d554013cbc5fd930849
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2022-12-06 13:56:27 +0100

    fixed focus on buttons

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,56 @@
         class="block fixed inset-x-0 bottom-0 z-10 bg-white dark:bg-slate-800 shadow"
       >
         <div class="flex justify-evenly">
-          <div
-            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px] border-white dark:border-slate-800"
+          <button
+            @click="onMenuTo(1)"
+            :class="
+              activeButton1
+                ? 'border-wd-green dark:border-wd-green'
+                : 'border-white dark:border-slate-800'
+            "
+            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px]"
           >
             <router-link to="/"><HomeIcon></HomeIcon> </router-link>
-          </div>
-          <div
-            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px] border-white dark:border-slate-800"
+          </button>
+          <button
+            @click="onMenuTo(2)"
+            :class="
+              activeButton2
+                ? 'border-wd-green dark:border-wd-green'
+                : 'border-white dark:border-slate-800'
+            "
+            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px]"
           >
             <router-link to="/form">
               <UserIcon></UserIcon>
             </router-link>
-          </div>
-          <div
-            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px] border-white dark:border-slate-800"
+          </button>
+          <button
+            @click="onMenuTo(3)"
+            :class="
+              activeButton3
+                ? 'border-wd-green dark:border-wd-green'
+                : 'border-white dark:border-slate-800'
+            "
+            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px]"
           >
             <router-link to="/documents"
               ><DocumentsIcon></DocumentsIcon>
             </router-link>
-          </div>
-          <div
-            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px] border-white dark:border-slate-800"
+          </button>
+          <button
+            @click="onMenuTo(4)"
+            :class="
+              activeButton4
+                ? 'border-wd-green dark:border-wd-green'
+                : 'border-white dark:border-slate-800'
+            "
+            class="dark:hover:border-wd-green hover:border-wd-green border-t-[3px]"
           >
             <router-link to="/calendar">
               <NotificationsIcon></NotificationsIcon
             ></router-link>
-          </div>
+          </button>
           <div
             class="flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
           ></div>
@@ -55,6 +79,10 @@ const isLoggedIn = ref(true);
 const router = useRouter();
 const auth = getAuth();
 const darkLightMode = ref(JSON.parse(localStorage.getItem("theme")));
+let activeButton1 = ref(true);
+let activeButton2 = ref(false);
+let activeButton3 = ref(false);
+let activeButton4 = ref(false);
 if (JSON.parse(localStorage.getItem("theme")) == "dark") {
   darkLightMode.value = "dark";
 } else {
@@ -82,6 +110,34 @@ const swichtDarkLightMode = () => {
     darkLightMode.value = "dark";
   } else {
     darkLightMode.value = "light";
+  }
+};
+const onMenuTo = (index) => {
+  switch (index) {
+    case 1:
+      activeButton1.value = true;
+      activeButton2.value = false;
+      activeButton3.value = false;
+      activeButton4.value = false;
+      break;
+    case 2:
+      activeButton1.value = false;
+      activeButton2.value = true;
+      activeButton3.value = false;
+      activeButton4.value = false;
+      break;
+    case 3:
+      activeButton1.value = false;
+      activeButton2.value = false;
+      activeButton3.value = true;
+      activeButton4.value = false;
+      break;
+    case 4:
+      activeButton1.value = false;
+      activeButton2.value = false;
+      activeButton3.value = false;
+      activeButton4.value = true;
+      break;
   }
 };
 </script>

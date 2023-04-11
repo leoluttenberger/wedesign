1416daab114f3299d27dbd6cd6106069a89705f3
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2023-04-11 13:42:27 +0200

    added archive db

--- src/views/ArchiveView.vue
+++ src/views/ArchiveView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overflow-auto overflow-scroll w-screen h-screen py-20 z-10">
     <section class="z-0">
-      <div v-for="(item, index) in applications" :key="index" class="p-2">
+      <div v-for="(item, index) in archives" :key="index" class="p-2">
         <div class="grow px-2">
           <div
             class="p-2 bg-white dark:bg-slate-800 text-black text-left dark:text-white font-Montserrat rounded-md border border-wd-green"
@@ -35,26 +35,7 @@
               </div>
             </div>
             <div>{{ item[0].job }}</div>
-            <div class="flex">
-              <div class="flex-none">Motivationsschreiben</div>
-              <div class="grow py-2 px-2">
-                <ArrowIcon
-                  class="dark:stroke-wd-white stroke-1 w-full h-2"
-                ></ArrowIcon>
-              </div>
-              <div class="flex-none">
-                <CloseIcon
-                  v-if="!item[0].mv"
-                  class="w-24 stroke-wd-error"
-                ></CloseIcon>
-              </div>
-              <div class="flex-none">
-                <CheckIcon
-                  v-if="item[0].mv"
-                  class="w-24 stroke-wd-green"
-                ></CheckIcon>
-              </div>
-            </div>
+            <div>{{ item[0].date }}</div>
           </div>
         </div>
       </div>
@@ -66,5 +47,5 @@ import { ref } from "vue";
 import ArrowIcon from "@/assets/icons/ArrowIcon.vue";
 import CloseIcon from "@/assets/icons/CloseIcon.vue";
 import CheckIcon from "@/assets/icons/CheckIcon.vue";
-const applications = ref(JSON.parse(localStorage.getItem("applications")));
+const archives = ref(JSON.parse(localStorage.getItem("archives")));
 </script>

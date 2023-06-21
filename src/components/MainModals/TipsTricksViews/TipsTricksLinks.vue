daccc53f887ac8660f291be015fbef728cbc9ae1
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2023-06-21 20:58:23 +0200

    updated links

--- src/components/MainModals/TipsTricksViews/TipsTricksLinks.vue
+++ src/components/MainModals/TipsTricksViews/TipsTricksLinks.vue
@@ -1,176 +1,132 @@
 <template>
-  <div class="text-black">
-    <vue-collapsible-panel-group>
-      <vue-collapsible-panel :expanded="false">
-        <template #title>Berufsorientierung allgemein </template>
-        <template #content
-          ><ul>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.berufslexikon.at/suche/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.berufslexikon.at/suche/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.berufskompass.at/berufskompass/
-"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.berufskompass.at/berufskompass/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.jopsy.at/intro-video
-"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.jopsy.at/intro-video
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.whatchado.com/de/
-
-"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.whatchado.com/de/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.kununu.com/at
-
-"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.kununu.com/at
-              </a>
-            </li>
-          </ul>
-        </template>
-      </vue-collapsible-panel>
-      <vue-collapsible-panel :expanded="false">
-        <template #title>Stellensuche </template>
-        <template #content
-          ><ul>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://jobs.ams.at/public/emps/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://jobs.ams.at/public/emps/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.ams.at/arbeitsuchende/arbeitslos-was-tun/lehrstellenboerse "
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.ams.at/arbeitsuchende/arbeitslos-was-tun/lehrstellenboerse
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://lehrbetriebsuebersicht.wko.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://lehrbetriebsuebersicht.wko.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.karriere.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.karriere.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.stepstone.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.stepstone.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.jobs.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.jobs.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.metajob.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.metajob.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://hokify.at/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://hokify.at/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://bund.jobboerse.gv.at/sap/bc/jobs/ "
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://bund.jobboerse.gv.at/sap/bc/jobs/
-              </a>
-            </li>
-          </ul>
-        </template>
-      </vue-collapsible-panel>
-      <vue-collapsible-panel :expanded="false">
-        <template #title>Bewerbung </template>
-        <template #content
-          ><ul>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.ams.at/arbeitsuchende/topicliste/bewerbungsvorlage"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.ams.at/arbeitsuchende/topicliste/bewerbungsvorlage
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.akyoung.at/arbeit/bewerbung/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.akyoung.at/arbeit/bewerbung/
-              </a>
-            </li>
-            <li class="hover:text-blue-500 underline hover:underline-offset-2">
-              <a
-                href="https://www.playmit.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >https://www.playmit.com/
-              </a>
-            </li>
-          </ul>
-        </template>
-      </vue-collapsible-panel>
-    </vue-collapsible-panel-group>
-  </div>
+  <TwAccordion
+    class="bg-white dark:bg-slate-700 rounded overflow-hidden"
+    :data="accordion"
+    keep-alive
+  >
+    <template
+      v-for="item in accordion"
+      :key="item.ref"
+      #[`header_${item.ref}`]="{ toggleAccordion, ref, isActive }"
+    >
+      <div
+        class="text-black bg-gray-300 dark:text-white dark:bg-slate-600 p-4 flex justify-between items-center cursor-pointer"
+        @click="toggleAccordion(ref)"
+      >
+        <div>{{ item.header }}</div>
+        <button class="flex items-center">
+          <TwFeather
+            class="transition-all duration-400"
+            type="chevron-down"
+            :class="{
+              'rotate-180': isActive,
+            }"
+          ></TwFeather>
+        </button>
+      </div>
+      <Transition duration="550" name="nested">
+        <div v-if="isActive">
+          <div>
+            <div
+              class="p-4 bg-wd-background text-black dark:text-white dark:bg-slate-500 hover:text-blue-500 underline hover:underline-offset-2"
+              :key="`${item.ref}`"
+              :ref="item.ref"
+            >
+              <ul>
+                <li
+                  v-for="(n, index) in item.body"
+                  v-bind:key="item.body[index]"
+                >
+                  <a
+                    :href="item.body[index]"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >{{ item.body[index] }}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Transition>
+    </template>
+  </TwAccordion>
 </template>
+
 <script setup lang="ts">
-import {
-  VueCollapsiblePanelGroup,
-  VueCollapsiblePanel,
-} from "@dafcoe/vue-collapsible-panel";
-import "@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css";
+import { ref, watch } from "vue";
+import { TwAccordion, TwFeather } from "vue3-tailwind";
+const show = ref(true);
+
+const accordion = ref([
+  {
+    ref: "Berufsorientierung allgemein",
+    header: "Berufsorientierung allgemein",
+    body: [
+      "https://www.berufslexikon.at/suche/",
+      "https://www.berufskompass.at/berufskompass/",
+      "https://www.jopsy.at/intro-video",
+      "https://www.whatchado.com/de/",
+      "https://www.kununu.com/at",
+    ],
+  },
+  {
+    ref: "Stellensuche",
+    header: "Stellensuche",
+    body: [
+      "https://jobs.ams.at/public/emps/",
+      "https://www.ams.at/arbeitsuchende/arbeitslos-was-tun/lehrstellenboerse",
+      "https://lehrbetriebsuebersicht.wko.at/",
+      "https://www.karriere.at/",
+      "https://www.stepstone.at/",
+      "https://www.jobs.at/",
+      "https://www.metajob.at/",
+      "https://hokify.at/",
+      "https://bund.jobboerse.gv.at/sap/bc/jobs/",
+    ],
+  },
+  {
+    ref: "Bewerbung",
+    header: "Bewerbung",
+    body: [
+      "https://www.ams.at/arbeitsuchende/topicliste/bewerbungsvorlage",
+      "https://www.akyoung.at/arbeit/bewerbung/",
+      "https://www.playmit.com/",
+    ],
+  },
+]);
+
+const isActiveCheck = ref("");
+
+const toggleAccordion = (ref) => {
+  isActiveCheck.value = isActiveCheck.value === ref ? "" : ref;
+};
+
+watch(isActiveCheck, (newValue, oldValue) => {
+  if (newValue !== "") {
+    const element = document.getElementById(newValue);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
 </script>
+
+<style scoped>
+.nested-enter-active {
+  transition: all 0.5s ease-in-out;
+}
+.nested-leave-active {
+  transition: all 0.1s ease-in-out;
+}
+/* delay leave of parent element */
+.nested-leave-active {
+  transition-delay: 0.5s;
+}
+
+.nested-enter-from,
+.nested-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
+</style>

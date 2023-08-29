0148951619b922710dc9dd50e020ea3d0a262e17
Leo Luttenberger <leo.luttenberger@univie.ac.at>
2023-08-29 14:19:11 +0200

    changed share query

--- src/components/MainModals/UserInfoViews/UserInfoPreview.vue
+++ src/components/MainModals/UserInfoViews/UserInfoPreview.vue
@@ -172,71 +172,69 @@ const saveAndDownLoadDocs = async () => {
     var isFileSaverSupported = !!new Blob();
     saveAs(pdf.value.output("blob"), fileNamePDF);
     saveAs(downloadDocx.value, fileNameDoc);
-  } catch (e) {
-    console.log("File Save not supported on this platform");
-  }
-
-  let sharePath;
-  await Filesystem.writeFile({
-    path: fileNameDoc,
-    data: base64StringDoc,
-    directory: Directory.Documents,
-  })
-    .then(
-      () => {
-        Filesystem.getUri({
-          directory: Directory.Documents,
-          path: fileNameDoc,
-        }).then(
-          (result) => {
-            Share.share({
-              title: fileNameDoc,
-              text: fileNameDoc,
-              files: [result.uri],
-            });
-            console.log("sharePath", result.uri);
-          },
-          (err) => {
-            console.log(err);
-          }
-        );
-      },
-      (err) => {
-        console.log(err);
-      }
-    )
-    .then(() => {
-      console.log("File written to document docx directory!");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  await nextTick();
-  const fileSize = pdf.value.output("blob").size / 1024;
-  console.log("Pdf-size", fileSize + " KB");
-
-  if (fileSize <= 2000) {
-    await nextTick();
     await Filesystem.writeFile({
-      path: `${fileNamePDF}`,
-      data: base64StringPdf,
+      path: fileNameDoc,
+      data: base64StringDoc,
       directory: Directory.Documents,
     })
+      .then(
+        () => {
+          Filesystem.getUri({
+            directory: Directory.Documents,
+            path: fileNameDoc,
+          }).then(
+            (result) => {
+              Share.share({
+                title: fileNameDoc,
+                text: fileNameDoc,
+                files: [result.uri],
+              });
+              console.log("sharePath", result.uri);
+            },
+            (err) => {
+              console.log(err);
+            }
+          );
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
       .then(() => {
-        console.log("File written to document pdf directory!");
+        console.log("File written to document docx directory!");
       })
       .catch((error) => {
         console.error(error);
       });
-  } else {
-    console.log("Error PDF filesize to big!");
+    await nextTick();
+    const fileSize = pdf.value.output("blob").size / 1024;
+    console.log("Pdf-size", fileSize + " KB");
+
+    if (fileSize <= 3000) {
+      await nextTick();
+      await Filesystem.writeFile({
+        path: `${fileNamePDF}`,
+        data: base64StringPdf,
+        directory: Directory.Documents,
+      })
+        .then(() => {
+          console.log("File written to document pdf directory!");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      console.log("Error PDF filesize to big!");
+    }
+  } catch (e) {
+    console.log("File Save not supported on this platform");
   }
 
-  if (Share.share && sharePath != "") {
+  if (Share.share) {
     await Share.share({
       title: "Share File",
       text: fileNameDoc,
-      url: sharePath,
+      url: "",
     });
   } else {
     // Fallback for browsers that do not support Web Share API
